823db277afbe877f12dc90fbe18da011ccb6bdcc
Giuliano Sposito <gsposito@ciandt.com>
2020-02-11 18:17:08 -0300

    Entendendo os dados das estacoes no brazil plotando num map

--- R/berkeley_data/berkeley_explore.R
+++ R/berkeley_data/berkeley_explore.R
@@ -14,12 +14,36 @@ temp_data <- readr::read_delim(file="./data/data.txt", skip = 111, delim = "\t",
 # Time Zone, WMO ID, Coop ID, WBAN ID, ICAO ID, # of Relocations, # Suggested Relocations,
 # of Sources, Hash
 
-readr::read_delim(file="./data/site_detail.txt", skip=149, delim="\t", 
-                  col_names = F, na=c("-99","-9999"), 
+site_data <- readr::read_delim(file="./data/site_detail.txt", skip=149, delim="\t", 
+                  col_names = F, 
+                  na=c("-99","-9999","[Missing]                               ",""), 
                   col_types = "icnnnnnncccciiiiiiic") %>% 
-  set_names(c("station_id","station_name","latitude","longitudo","elevation","lat_uncert",
+  set_names(c("station_id","station_name","latitude","longitude","elevation","lat_uncert",
               "lon_uncert","elev_uncert","country","state","county","timezone","wmo_id",
               "coop_id","wban_id","icao_id","relocations","suggested_relocations","sources","hash")) %>% 
-  glimpse()
+  mutate_if(is.character, str_trim) %>% 
+  mutate_at(vars(country, state, county), as.factor)
+
+brazil_sites <- site_data %>% 
+  filter(country=="Brazil")
+
+library(yaml)
+library(ggmap)
+library(ggrepel)
+
+google <- yaml::read_yaml("./config/google_keys.yaml")
+ggmap::register_google(google$key)
+
+bbox <- make_bbox(lon = brazil_sites$longitude, lat = brazil_sites$latitude, f = .1)
+gmap <- get_map(location=bbox, source="google", maptype="terrain")
+
+ggmap(gmap) +
+  geom_point(data=brazil_sites, aes(x=longitude, y=latitude)) +
+  geom_text_repel(data=brazil_sites, aes(x=longitude, y=latitude, label=station_name), 
+                  size=3) +
+  theme_void()
+  +#geom_text(data=brazil_sites, aes()) +
+  
 
 
+geom_text
