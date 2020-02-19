23334bde78db40dd1d4e7ff197ec4cfb81bcba5e
Giuliano Sposito <gsposito@ciandt.com>
2020-02-19 10:13:43 -0300

    testes do strips, maps e slope x R2

--- R/berkeley_data/berkeley_explore.R
+++ R/berkeley_data/berkeley_explore.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(broom)
+library(ggrepel)
 
 temp_data <- readr::read_delim(file="./data/data.txt", skip = 111, delim = "\t",
                   col_names = F, col_types = "iicnnii", na = "-99") %>% 
@@ -40,10 +41,63 @@ gmap <- get_map(location=bbox, source="google", maptype="terrain")
 ggmap(gmap) +
   geom_point(data=brazil_sites, aes(x=longitude, y=latitude)) +
   geom_text_repel(data=brazil_sites, aes(x=longitude, y=latitude, label=station_name), 
-                  size=3) +
+                  size=2) +
   theme_void()
-  +#geom_text(data=brazil_sites, aes()) +
-  
 
 
-geom_text
+br_temp <- brazil_sites %>% 
+  inner_join(temp_data, by = "station_id") %>% 
+  select(station_id, station_name, latitude, longitude, 
+         elevation, country, year, temperature)
+
+br_temp %>% 
+  group_by(country, year) %>% 
+  summarise( temp_avg = mean(temperature, na.rm = T)) %>% 
+  ungroup() %>% 
+  filter(year>=max(year)-100) %>% 
+  mutate(temp_avg=temp_avg-mean(temp_avg)) %>% 
+  # ggplot(aes(x=year, y=temp_avg)) +
+  # geom_point() + 
+  # theme_minimal()
+  ggplot(aes(x=year, fill=temp_avg, color=temp_avg)) +
+    geom_bar(aes(y=1), stat="identity",
+             position = position_dodge2(width = 0, padding=0)) +
+    scale_fill_gradient2(low="darkblue", mid="lightblue", high="red") +
+    scale_color_gradient2(low="darkblue", mid="lightblue", high="red") +
+    theme_void() +
+    theme(
+      legend.position = "none",
+      panel.border = element_blank()
+    )
+
+br_temp %>% 
+  group_by(station_id, station_name, year) %>%
+  top_n(year,100) %>% 
+  summarise(temp_avg=mean(temperature, rm.na=T)) %>% 
+  nest() %>% 
+  mutate(model=map(data, function(.x){
+    lm(temp_avg~year, .x)
+  })) %>% 
+  mutate(m.values=map(model, function(.x){
+    bind_cols(
+      .x %>% 
+        tidy() %>% 
+        filter(term=="year") %>% 
+        select(slope=estimate),
+      .x %>% 
+        glance() %>% 
+        select(r.squared)  
+    ) %>% return()
+  })) %>% 
+  unnest(m.values) %>% 
+  ungroup() %>% 
+  ggplot(aes(y=slope, x=r.squared)) +
+  geom_point() +
+  geom_text_repel(aes(label=station_name), size=1.75) +
+  theme_minimal()
+
+
+
+
+
+
